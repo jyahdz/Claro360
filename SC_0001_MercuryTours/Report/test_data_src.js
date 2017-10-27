c59fc2ed42bbdfe8a433a1400021edb98e507118
jyahdz <jyahdz@gmail.com>
2017-10-27 13:52:07 -0500

    UFT commit

--- SC_0001_MercuryTours/Report/test_data_src.js
+++
@@ -1,1 +0,0 @@
-﻿var __testDataSource={items:[{dataFilePath:'Default.xls',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\jonathan.arredondo\\Documents\\GitHub\\Claro360\\SC_0001_MercuryTours',namePairs:[]}]};
